2c17526567ab4bd5a0a44aafc7b700540f1be3c6
Jeovani Stars <jeovanigamati@gmail.com>
2025-06-10 10:26:33 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/pgr03jeovaningamamaven/Pgr03jeovaningamamaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pgr03jeovaningamamaven;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Pgr03jeovaningamamaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
